8ba3a92791b77223eadda6cf3e3687ff5dae40ac
RTNP <65603675+RTNP@users.noreply.github.com>
2020-05-19 14:31:33 +0200

    Initial Upload

---
+++ SharpEnable_WDigest/SharpEnable_WDigest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Enable_WDigest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.RegistryKey key;
+            //Set Key Location
+            key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest", true);
+            //Create a new key with set value's
+            key.SetValue("UseLogonCredential", "1", RegistryValueKind.DWord);
+            //Close Key
+            key.Close();
+        }
+    }
+}
